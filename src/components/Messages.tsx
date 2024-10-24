d15dad659f5aae9bb806a33950b737d63ff47de7
ronen ram <ronenram1234@gmail.com>
2024-10-24 15:15:08 +0300

    update

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -8,7 +8,9 @@ export default function Messages() {
   return (
     <div>
       {messages.map((msg, index) => {
+        
         if (msg.type === "user_message" || msg.type === "assistant_message") {
+          console.log(`msg-${msg.type} ${msg.message.role} ${msg.message.content}   index-${index}`)
           return (
             <div key={msg.type + index}>
               <div>{msg.message.role === "user" ? "User" : "Assistant"}:</div>
